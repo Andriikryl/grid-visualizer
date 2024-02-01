a1969c70e8f64837f1ce13941d12b5ad13e65857
Andriikryl <andreyk557@gmail.com>
2024-02-01 15:16:43 +0200

    add RadioGroup component

---
+++ src/components/RadioGroup.svelte
@@ -0,0 +1,31 @@
+<script>
+  let currentTemplate;
+  let templates;
+</script>
+
+<form class="form">
+  {#each templates as template}
+    <label>
+      <input
+        type="radio"
+        name="flavours"
+        value={template}
+        bind:group={currentTemplate}
+      />
+      {template}
+    </label>
+  {/each}
+</form>
+
+<style>
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-block-end: 20px;
+    background-color: #fefae0;
+    padding: 20px;
+    border-radius: 10px;
+    max-width: 300px;
+  }
+</style>
